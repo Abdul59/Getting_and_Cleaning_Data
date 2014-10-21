6e5ae92ee872a5fcd8acec64fa43018b2d858b81
Abdul59 <Abdul59@users.noreply.github.com>
2014-10-21 20:05:28 +0300

    Update ForTidyData.R

--- ForTidyData.R
+++ ForTidyData.R
@@ -6,15 +6,15 @@
 
 Tmp1 <- read.table("train/X_train.txt")
 Tmp2 <- read.table("test/X_test.txt")
-Xx <- rbind(tmp1, tmp2)
+Xx <- rbind(Tmp1, Tmp2)
 
 Tmp1 <- read.table("train/subject_train.txt")
 Tmp2 <- read.table("test/subject_test.txt")
-Ss <- rbind(tmp1, tmp2)
+Ss <- rbind(Tmp1, Tmp2)
 
 Tmp1 <- read.table("train/y_train.txt")
 Tmp2 <- read.table("test/y_test.txt")
-Yy <- rbind(tmp1, tmp2)
+Yy <- rbind(Tmp1, Tmp2)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 
@@ -29,7 +29,7 @@ names(Xx) <- tolower(names(Xx))
 
 activities <- read.table("activity_labels.txt")
 activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
-Yy[,1] = activities[Y[,1], 2]
+Yy[,1] = activities[Yy[,1], 2]
 names(Yy) <- "activity"
 
 # 4. Appropriately labels the data set with descriptive activity names.
@@ -51,9 +51,12 @@ for (s in 1:numSubjects) {
   for (a in 1:numActivities) {
     result[row, 1] = uniqueSubjects[s]
     result[row, 2] = activities[a, 2]
-    tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
-    result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+    Tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
+    result[row, 3:numCols] <- colMeans(Tmp[, 3:numCols])
     row = row+1
   }
 }
 write.table(result, "data_set_with_the_averages.txt")
+
+
+
